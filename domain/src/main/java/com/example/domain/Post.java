ed24a026a68326130fcb4dc715ee22844072166f
ryudung <rhkdgus221@naver.com>
2018-11-15 03:07:17 +0900

    부트2.x  하이버네이트5.x 기본 Generate 전략 변경에 대한 대응.

--- domain/src/main/java/com/example/domain/Post.java
+++ domain/src/main/java/com/example/domain/Post.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
 @Entity
 public class Post {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
